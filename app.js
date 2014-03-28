d1e7d9794240a995f4dd1b7d92cd0934705a8433
Nick Nance <nance.nick@gmail.com>
2014-03-28 10:38:44 -0500

    add node serving of static pages

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.use(express.compress());
 app.use(express.static(__dirname + '/dist'));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
 
 app.listen(process.env.PORT || 3000);
